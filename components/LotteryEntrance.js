475afe509de58a1580e15fb5900a0234bb9c330c
PatrickAlphac <54278053+PatrickAlphaC@users.noreply.github.com>
2022-04-14 07:05:00 -0400

    update

--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -1,25 +1,25 @@
 import { useWeb3Contract } from "react-moralis"
 import { contractAddresses, abi } from "../constants"
 // dont export from moralis when using react
-import { useMoralis, useNativeBalance } from "react-moralis"
+import { useMoralis } from "react-moralis"
 import { useEffect, useState } from "react"
 import { useNotification } from "web3uikit"
+import { ethers } from "ethers"
 
 export default function LotteryEntrance() {
-    const dispatch = useNotification()
-
     const { Moralis, isWeb3Enabled, chainId: chainIdHex } = useMoralis()
     // These get re-rendered every time due to our connect button!
     const chainId = parseInt(chainIdHex)
     // console.log(`ChainId is ${chainId}`)
     const raffleAddress = chainId in contractAddresses ? contractAddresses[chainId][0] : null
-    const options = { abi, contractAddress: raffleAddress }
 
-    // State hooks for those that
+    // State hooks
     // https://stackoverflow.com/questions/58252454/react-hooks-using-usestate-vs-just-variables
     const [entranceFee, setEntranceFee] = useState("0")
     const [numberOfPlayers, setNumberOfPlayers] = useState("0")
     const [recentWinner, setRecentWinner] = useState("0")
+
+    const dispatch = useNotification()
 
     const {
         runContractFunction: enterRaffle,
@@ -31,6 +31,15 @@ export default function LotteryEntrance() {
         contractAddress: raffleAddress,
         functionName: "enterRaffle",
         msgValue: entranceFee,
+        params: {},
+    })
+
+    /* View Functions */
+
+    const { runContractFunction: getEntranceFee } = useWeb3Contract({
+        abi: abi,
+        contractAddress: raffleAddress, // specify the networkId
+        functionName: "getEntranceFee",
         params: {},
     })
 
@@ -49,17 +58,18 @@ export default function LotteryEntrance() {
     })
 
     async function updateUIValues() {
-        const fee = await Moralis.executeFunction({
-            functionName: "getEntranceFee",
-            ...options,
-        })
-        setEntranceFee(fee.toString())
-        // console.log(`EntranceFee is ${fee.toString()}`)
-        const numPlayer = await getPlayersNumber()
-        setNumberOfPlayers(numPlayer.toString())
-
-        const recentWinner = await getRecentWinner()
-        setRecentWinner(recentWinner.toString())
+        // Another way we could make a contract call:
+        // const options = { abi, contractAddress: raffleAddress }
+        // const fee = await Moralis.executeFunction({
+        //     functionName: "getEntranceFee",
+        //     ...options,
+        // })
+        const entranceFeeFromCall = (await getEntranceFee()).toString()
+        const numPlayersFromCall = (await getPlayersNumber()).toString()
+        const recentWinnerFromCall = await getRecentWinner()
+        setEntranceFee(entranceFeeFromCall)
+        setNumberOfPlayers(numPlayersFromCall)
+        setRecentWinner(recentWinnerFromCall)
     }
 
     useEffect(() => {
@@ -71,6 +81,7 @@ export default function LotteryEntrance() {
     // empty list means it'll run once after the initial rendering
     // and dependencies mean it'll run whenever those things in the list change
 
+    // An example filter for listening for events, we will learn more on this next Front end lesson
     // const filter = {
     //     address: raffleAddress,
     //     topics: [
@@ -105,6 +116,8 @@ export default function LotteryEntrance() {
                         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-auto"
                         onClick={async () =>
                             await enterRaffle({
+                                // onComplete:
+                                // onError:
                                 onSuccess: handleSuccess,
                             })
                         }
@@ -116,9 +129,9 @@ export default function LotteryEntrance() {
                             "Enter Raffle"
                         )}
                     </button>
+                    <div>Entrance Fee: {ethers.utils.formatUnits(entranceFee, "ether")} ETH</div>
                     <div>The current number of players is: {numberOfPlayers}</div>
                     <div>The most previous winner was: {recentWinner}</div>
-                    {/* How would I get how much value we have secured so far?  */}
                 </>
             ) : (
                 <div>Please connect to a supported chain </div>
